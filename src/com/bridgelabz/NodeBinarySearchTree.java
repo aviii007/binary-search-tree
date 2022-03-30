8b5372b6855052d0d71942980610fb404a73e16a
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-30 20:54:10 +0530

    Added the Search method.

--- src/com/bridgelabz/NodeBinarySearchTree.java
+++ src/com/bridgelabz/NodeBinarySearchTree.java
@@ -25,5 +25,22 @@ public class NodeBinarySearchTree {
         root.right = null;
         return root;
     }
+    public boolean searchNode(Node node,int data){
+        if (node == null) return false;
+        boolean isPresent = false;
+        while (node != null){
+            if (data < node.data){
+                node = node.left;
+            }
+            else if(data > node.data){
+                node=node.right;
+            }
+            else {
+                isPresent = true;
+                break;
+            }
+        }
+        return  isPresent;
+    }
 
 }
