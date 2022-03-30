e36f2c6e1d6415fe5ed0bdf19a5b7f045dae9017
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-30 20:51:07 +0530

    Added the Insert method.

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -1,8 +1,10 @@
 package com.bridgelabz;
 
-public class Main {
+public class BinarySearchTree {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Binary Search tree program");
+        NodeBinarySearchTree tree = new NodeBinarySearchTree();
+        Node node = new Node();
     }
 }
